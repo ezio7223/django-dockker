130f2f10404d37073199691c5949f1920c0e8faa
ezio7223 <95769190+ezio7223@users.noreply.github.com>
2025-02-09 16:40:36 +0530

    2nd commit

--- MyApp/settings.py
+++ MyApp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(*lst^&0&z64&d-ken@%%lrda-nq@xk0#zb*l-rzx)d-)*&vrg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

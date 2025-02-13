f1e96333c42b1574419d1cd532e76942f323f9e9
ezio7223 <95769190+ezio7223@users.noreply.github.com>
2025-02-13 23:37:07 +0530

    13/2- 2nd Commit

--- polls/views.py
+++ polls/views.py
@@ -3,8 +3,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index. personel web page")
-# Create your views here.
-
-def index(request):
     return render(request, 'polls/index.html')
